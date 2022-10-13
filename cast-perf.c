75899ad14471be93e12622d8012364e5f2af1176
최종인 <ekejaqutmqk@gmail.com>
2022-10-14 03:56:22 +0900

    pblk* -> void*

--- cast-perf.c
+++ cast-perf.c
@@ -8,37 +8,14 @@
 /*                                                                            */
 /******************************************************************************/
 
-#include <linux/string.h>
-#include <linux/jiffies.h>	//time
-
 #include "cast-perf.h"
 
-
-/* Creator for CAST_PERF */
-void CAST_SET_PERF(struct pblk *pblk)
-{
-	// member variables
-	pblk->c_perf.active		= 0;
-	pblk->c_perf.unit_time	= 0;
-	pblk->c_perf.next_time	= 0;
-	pblk->c_perf.read		= 0;
-	pblk->c_perf.write		= 0;
-
-	// method
-	pblk->c_perf.init 				= &CAST_PERF_INIT
-	pblk->c_perf.create_data_file	= &CAST_CREATE_DATA_FILE
-	pblk->c_perf.close_data_file	= &CAST_CLOSE_DATA_FILE
-	pblk->c_perf.write_in_data_file	= &CAST_WRITE_TO_DATA_FILE
-	pblk->c_perf.flush_thread		= &CAST_FLUSH_DATA_TO_FILE_THREAD
-
-	pblk->c_perf.increase_read		= &CAST_INCREASE_READ
-	pblk->c_perf.increase_write		= &CAST_INCREASE_WRIGHT
-	pblk->c_perf.reset_count		= &CAST_RESET_COUNT
-}
+/**************** CAST ***************/
 
 /* preparing measurment */
-void CAST_PERF_INIT(struct pblk *pblk)
+void CAST_PERF_INIT(void *private)
 {
+	struct pblk *pblk = private;
 	pblk->c_perf.create_data_file(pblk);
 	pblk->c_perf.next_time = get_jiffies_64();
 	pblk->c_perf.reset_count(pblk);
@@ -47,51 +24,59 @@ void CAST_PERF_INIT(struct pblk *pblk)
 	pblk->c_perf.active = 1;
 
 	//flush thread start
-	pblk->c_perf.flush_thread(pblk);
+	printk(KERN_ALERT "[  CAST  ] - flush thread of %s is run\n", pblk->disk->disk_name);
+	pblk->c_perf.thead = (struct task_struct*)kthread_run((pblk->c_perf.flush_thread), 
+		pblk, "flush_thread");
+	printk(KERN_ALERT "[  CAST  ] - %s is start\n", pblk->disk->disk_name);
 }
 
 /* open data file */
-int CAST_CREATE_DATA_FILE(struct pblk *pblk)
+int CAST_CREATE_DATA_FILE(void *private)
 {
+	struct pblk *pblk = private;
 	char filename[256];
 
 	// set file name
-	sprintf(filename,"/pblk-cast_perf/%s.data",pblk->disk->name);
+	sprintf(filename,"/pblk-cast_perf/%s.data",pblk->disk->disk_name);
 
 	/*open the file */
-	pblk->c_perf.data_file = filp_open(filename, O_WRONLY, 0);
-	if (IS_ERR(fp))
+	pblk->c_perf.data_file = filp_open(filename, O_WRONLY | O_CREAT, 0);
+	if (IS_ERR(pblk->c_perf.data_file))
 	{
-		printk(KERN_ALERT "[  CAST  ] - Cannot open the file %s %ld\n", filename, PTR_ERR(fp));
+		printk(KERN_ALERT "[  CAST  ] - Cannot open the file %s %ld\n", 
+			filename, PTR_ERR(pblk->c_perf.data_file));
 		return -1;
 	}
 
 	return 0;
 }
 /* close data file */
-int CAST_CLOSE_DATA_FILE(struct pblk *pblk)
+int CAST_CLOSE_DATA_FILE(void *private)
 {
-	if (IS_ERR(fp))
+	struct pblk *pblk = private;
+	if (IS_ERR(pblk->c_perf.data_file))
 	{
-		printk(KERN_ALERT "[  CAST  ] - data file for %s is not opened\n", pblk->disk->name);
+		printk(KERN_ALERT "[  CAST  ] - data file for %s is not opened\n", pblk->disk->disk_name);
 		return -1;
 	}
 
 	filp_close(pblk->c_perf.data_file, NULL);
-	printk(KERN_ALERT "[  CAST  ] - end measuring %s\n", pblk->disk->name);
+	printk(KERN_ALERT "[  CAST  ] - End measuring %s\n", pblk->disk->disk_name);
 
 	//end logging
 	pblk->c_perf.active = 0;
+	kthread_stop(pblk->c_perf.thead);
 	return 0;
 }
 
 /* memory -> data file */
-int CAST_WRITE_TO_DATA_FILE(struct pblk *pblk, int time)
+int CAST_WRITE_TO_DATA_FILE(void *private, int time)
 {
+	struct pblk *pblk = private;
 	char str[256];
-	if (IS_ERR(fp))
+	if (IS_ERR(pblk->c_perf.data_file))
 	{
-		printk(KERN_ALERT "[  CAST  ] - data file for %s is not opened\n", pblk->disk->name);
+		printk(KERN_ALERT "[  CAST  ] - data file for %s is not opened\n", pblk->disk->disk_name);
 		return -1;
 	}
 
@@ -100,33 +85,63 @@ int CAST_WRITE_TO_DATA_FILE(struct pblk *pblk, int time)
 }
 
 /* Call CAST_WRITE_TO_DATA_FILE() periodically */
-void CAST_FLUSH_DATA_TO_FILE_THREAD(struct pblk *pblk)
+int CAST_FLUSH_DATA_TO_FILE_THREAD(void *private)
 {
+	struct pblk *pblk = private;
 	long now = get_jiffies_64();
 	while (pblk->c_perf.active == 1)
 	{
 		if (now < pblk->c_perf.next_time)
 		{
-			pblk->c_perf.write_in_data_file(pblk, (int)(now / HZ))
+			pblk->c_perf.write_in_data_file(pblk, (int)(now / HZ));
 		}
 		msleep(pblk->c_perf.unit_time/10);
 	}
+
+	return 0;
 }
 
 /* increase IOPS by read */
-void CAST_INCREASE_READ(struct pblk *pblk)
+void CAST_INCREASE_READ(void *private)
 {
+	struct pblk *pblk = private;
 	pblk->c_perf.read++;
 }
 /* increase IOPS by write */
-void CAST_INCREASE_WRIGHT(struct pblk *pblk)
+void CAST_INCREASE_WRIGHT(void *private)
 {
+	struct pblk *pblk = private;
 	pblk->c_perf.write++;
 }
 /* clear IOPS count, update the next time */
-void CAST_RESET_COUNT(struct pblk *pblk)
+void CAST_RESET_COUNT(void *private)
 {
+	struct pblk *pblk = private;
 	pblk->c_perf.read	= 0;
 	pblk->c_perf.write	= 0;
-	pblk->c_perf.next_time = pblk->c_perf.next_time  + HZ*(pblk->c_perf.uint_time/100);
+	pblk->c_perf.next_time = pblk->c_perf.next_time  + (HZ*pblk->c_perf.unit_time)/1000;
 }
+
+/* Creator for CAST_PERF */
+void CAST_SET_PERF(void *private)
+{
+	struct pblk *pblk = private;
+	// member variables
+	pblk->c_perf.active		= 0;
+	pblk->c_perf.unit_time	= 0;
+	pblk->c_perf.next_time	= 0;
+	pblk->c_perf.read		= 0;
+	pblk->c_perf.write		= 0;
+
+	// method
+	pblk->c_perf.init 				= &CAST_PERF_INIT;
+	pblk->c_perf.create_data_file	= &CAST_CREATE_DATA_FILE;
+	pblk->c_perf.close_data_file	= &CAST_CLOSE_DATA_FILE;
+	pblk->c_perf.write_in_data_file	= &CAST_WRITE_TO_DATA_FILE;
+	pblk->c_perf.flush_thread		= &CAST_FLUSH_DATA_TO_FILE_THREAD;
+
+	pblk->c_perf.increase_read		= &CAST_INCREASE_READ;
+	pblk->c_perf.increase_write		= &CAST_INCREASE_WRIGHT;
+	pblk->c_perf.reset_count		= &CAST_RESET_COUNT;
+}
+/**************** CAST END ***************/
--- cast-perf.h
+++ cast-perf.h
@@ -13,59 +13,64 @@
 #include <linux/module.h> 
 #include <linux/kernel.h>
 
+#include <linux/string.h>
+#include <linux/jiffies.h>	//time
+#include <linux/delay.h>
+#include <linux/timer.h>	//msleep
+#include <linux/kthread.h>	//thread
 //file read
 #include <linux/file.h>
 #include <linux/fs.h>      // Needed by filp
 #include <asm/uaccess.h>   // Needed by segment descriptors
-
 typedef struct cast_perf
 {
 	struct file * data_file;	// log file
+	struct task_struct *thead; 	// flush thread
 
 	int 	active;			// 0:stop measuring, 1:measuring
 	int	 	unit_time;		// unit time to log file write
+	long 	init_time;		// initial time
 	long 	next_time;		// next file write
 	int 	read;			// read count per unit time
 	int 	write;			// write count per unit time
 
 	// like method
-	void (*init)(struct pblk *pblk);
-	int	 (*create_data_file)(struct pblk *pblk);
-	int  (*close_data_file)(struct pblk *pblk);
-	int  (*write_in_data_file)(struct pblk *pblk, int time);
-	void (*flush_thread)(struct pblk *pblk);
+	void (*init)(void *private);
+	int	 (*create_data_file)(void *private);
+	int  (*close_data_file)(void *private);
+	int  (*write_in_data_file)(void *private, int time);
+	int (*flush_thread)(void *private);
 
-	void (*increase_read)(struct pblk *pblk);
-	void (*increase_write)(struct pblk *pblk);
-	void (*reset_count)(struct pblk *pblk);
+	void (*increase_read)(void *private);
+	void (*increase_write)(void *private);
+	void (*reset_count)(void *private);
 
 }CAST_PERF;
 
-
 /* Creator for CAST_PERF */
-void CAST_SET_PERF(struct pblk *pblk));
+void CAST_SET_PERF(void *private);
 
 
 /*  TODO:remove this declarations  */
 
 // /* preparing measurment */
-// void CAST_PERF_INIT(struct pblk *pblk);
+// void CAST_PERF_INIT(void *private);
 // /* start measuring */
-// void CAST_MEASURING_START(struct pblk *pblk);
+// void CAST_MEASURING_START(void *private);
 // /* memory -> data file */
-// int CAST_WRITE_TO_DATA_FILE(struct pblk *pblk, int time);
+// int CAST_WRITE_TO_DATA_FILE(void *private, int time);
 // /* open data file */
-// int CAST_CREATE_DATA_FILE(struct pblk *pblk);
+// int CAST_CREATE_DATA_FILE(void *private);
 // /* close data file */
-// int CAST_CLOSE_DATA_FILE(struct pblk *pblk);
+// int CAST_CLOSE_DATA_FILE(void *private);
 // /* Call CAST_WRITE_TO_DATA_FILE() periodically */
-// void CAST_FLUSH_DATA_TO_FILE_THREAD(struct pblk *pblk);
+// void CAST_FLUSH_DATA_TO_FILE_THREAD(void *private);
 
 // /* increase IOPS by read */
-// void CAST_INCREASE_READ(struct pblk *pblk);
+// void CAST_INCREASE_READ(void *private);
 // /* increase IOPS by write */
-// void CAST_INCREASE_WRIGHT(struct pblk *pblk);
+// void CAST_INCREASE_WRIGHT(void *private);
 // /* clear IOPS count*/
-// void CAST_RESET_COUNT(struct pblk *pblk);
+// void CAST_RESET_COUNT(void *private);
 
 #endif /* CAST_PERF_H */
